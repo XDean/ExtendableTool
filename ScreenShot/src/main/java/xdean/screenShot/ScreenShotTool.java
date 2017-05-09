5f85b1aba2f25b690f9b509a064e2591e6fcd32d
XDean <373216024@qq.com>
2017-05-09 15:59:02 +0800

    pack system scope dependency correct enable screen shot use relative path in maven property

--- ScreenShot/src/main/java/xdean/screenShot/ScreenShotTool.java
+++ ScreenShot/src/main/java/xdean/screenShot/ScreenShotTool.java
@@ -4,25 +4,27 @@ import javafx.beans.binding.Bindings;
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.SimpleBooleanProperty;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import xdean.jex.config.Config;
 import xdean.tool.api.Tool;
 import xdean.tool.api.impl.AbstractToolItem;
 
 @Tool
+@Slf4j
 public class ScreenShotTool extends AbstractToolItem {
   private final String ENABLE_KEY = "ScreenShotEnable";
-  @Getter BooleanProperty enable;
+  @Getter
+  BooleanProperty enable;
 
   public ScreenShotTool() {
     super("");
-    enable = new SimpleBooleanProperty(
-        Config.getProperty(ENABLE_KEY)
-            .map(Boolean::valueOf)
-            .orElse(false));
+    enable = new SimpleBooleanProperty(false);
     enable.addListener((ob, o, n) -> {
       ScreenShot.register(n);
       Config.setProperty(ENABLE_KEY, n.toString());
+      log.debug((n ? "Enable" : "Disable") + "Screen Shot");
     });
+    enable.set(Config.getProperty(ENABLE_KEY).map(Boolean::valueOf).orElse(false));
     textProperty().bind(
         Bindings.when(enable)
             .then("Disable")
