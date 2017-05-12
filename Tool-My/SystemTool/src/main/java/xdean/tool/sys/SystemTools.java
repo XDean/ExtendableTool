07bdfdf9efe25dd576b942205520b3aa86dacaff
XDean <373216024@qq.com>
2017-05-12 18:37:48 +0800

    update commandtool

--- Tool-My/SystemTool/src/main/java/xdean/tool/sys/SystemTools.java
+++ Tool-My/SystemTool/src/main/java/xdean/tool/sys/SystemTools.java
@@ -1,7 +1,13 @@
 package xdean.tool.sys;
 
+import xdean.tool.api.IToolGetter;
 import xdean.tool.api.Tool;
+import xdean.tool.sys.other.CommandTool;
 
 @Tool(path = "System")
 public class SystemTools {
+
+  @Tool
+  public static final IToolGetter EXEPLORER_RESTART = () ->
+      CommandTool.create("RestartExplorer", "cmd", "/c", "taskkill", "/f", "/IM", "explorer.exe", "&&", "explorer.exe");
 }
--- Tool-My/SystemTool/src/main/java/xdean/tool/sys/other/CommandTool.java
+++ Tool-My/SystemTool/src/main/java/xdean/tool/sys/other/CommandTool.java
@@ -1,24 +1,47 @@
 package xdean.tool.sys.other;
 
-import lombok.AllArgsConstructor;
+import java.nio.charset.Charset;
+
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import rx.Observable;
+import rx.observables.StringObservable;
+import rx.schedulers.Schedulers;
 import xdean.tool.api.impl.AbstractToolItem;
 
-@AllArgsConstructor
+@Slf4j
 public class CommandTool extends AbstractToolItem {
 
-  public static CommandTool create(String text, String command) {
+  public static CommandTool create(String text, String... command) {
     CommandTool tool = new CommandTool(command);
     tool.textProperty().set(text);
     return tool;
   }
 
+  ProcessBuilder pb;
   String command;
+
+  protected CommandTool(String... command) {
+    pb = new ProcessBuilder(command);
+    this.command = String.join(" ", command);
+  }
 
   @SneakyThrows
   @Override
   public void onClick() {
-    Runtime.getRuntime().exec(command);
+    Process exec = pb.start();
+    Observable.merge(
+        StringObservable.from(exec.getInputStream())
+            .compose(o -> StringObservable.decode(o, Charset.defaultCharset()))
+            .single()
+            .doOnNext(s -> log.info("[{}] output: {}", command, s)),
+        StringObservable.from(exec.getErrorStream())
+            .compose(o -> StringObservable.decode(o, Charset.defaultCharset()))
+            .single()
+            .doOnNext(s -> log.error("[{}] error: {}", command, s)))
+        .subscribeOn(Schedulers.newThread())
+        .doOnCompleted(() -> log.info("[{}] completed.", command))
+        .subscribe();
   }
 
 }
