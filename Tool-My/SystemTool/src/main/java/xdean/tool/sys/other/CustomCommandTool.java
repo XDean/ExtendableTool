deef86e5525879acdd47671d390be29bf3a0d2f3
XDean <373216024@qq.com>
2017-08-29 16:34:36 +0800

    use jex0.0.4

--- Tool-My/SystemTool/src/main/java/xdean/tool/sys/other/CustomCommandTool.java
+++ Tool-My/SystemTool/src/main/java/xdean/tool/sys/other/CustomCommandTool.java
@@ -1,6 +1,6 @@
 package xdean.tool.sys.other;
 
-import xdean.tool.api.ITool;
+import xdean.tool.api.IToolGetter;
 import xdean.tool.api.Tool;
 import xdean.tool.api.impl.SimpleToolItem;
 import xdean.tool.sys.SystemTools;
@@ -8,5 +8,5 @@ import xdean.tool.sys.SystemTools;
 @Tool(parent = SystemTools.class, path = "Custom")
 public class CustomCommandTool {
   @Tool
-  ITool tool = new SimpleToolItem("Help", () -> System.out.println(123));
+  public static final IToolGetter tool = () -> new SimpleToolItem("Help", () -> System.out.println(123));
 }
