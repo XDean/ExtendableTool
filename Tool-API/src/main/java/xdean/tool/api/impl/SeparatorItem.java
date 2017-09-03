e8265fc49166e67cb94ab1a3a5fa8a8cabe87fe1
XDean <373216024@qq.com>
2017-09-03 17:10:42 +0800

    separator

--- Tool-API/src/main/java/xdean/tool/api/impl/SeparatorItem.java
+++ Tool-API/src/main/java/xdean/tool/api/impl/SeparatorItem.java
@@ -4,29 +4,45 @@ import java.net.URL;
 
 import javafx.beans.property.ListProperty;
 import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleListProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import javafx.collections.FXCollections;
 import xdean.tool.api.ITool;
 
 public enum SeparatorItem implements ITool {
   INSTANCE;
+  protected StringProperty text;
+  protected ObjectProperty<URL> iconUrl;
+  protected ObjectProperty<ITool> parent;
+  protected ListProperty<ITool> children;
+
+  private SeparatorItem() {
+    text = new SimpleStringProperty("SeparatorItem");
+    iconUrl = new SimpleObjectProperty<>();
+    parent = new SimpleObjectProperty<>();
+    children = new SimpleListProperty<>(FXCollections.observableArrayList());
+  }
+
   @Override
   public StringProperty textProperty() {
-    return null;
+    return text;
   }
 
   @Override
   public ObjectProperty<URL> iconUrlProperty() {
-    return null;
+    return iconUrl;
   }
 
   @Override
   public ListProperty<ITool> childrenProperty() {
-    return null;
+    return children;
   }
 
   @Override
   public ObjectProperty<ITool> parentProperty() {
-    return null;
+    return parent;
   }
 
   @Override
